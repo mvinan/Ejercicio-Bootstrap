4f534201e8183294ca686aa0ee7934fa2817f772
Miguel Alejandro <mvinan@icloud.com>
2015-03-22 21:03:18 -0500

    organizando bt-menu

--- js/index.js
+++ js/index.js
@@ -1,10 +1,42 @@
-$(".play").on("click", play);
+(function() {
 
-function play(){
-	if ( $(".play") == true ) {
-		$(".progreso").removeClass(".animar");
+	$play = $("[data-button='play']");
+	$stop = $("[data-button='stop']");
+
+	//botones y listener with events
+	$( $play ).on("click", goAndStop);
+	$( $stop ).on("click", stoped);
+
+
+	//La accion con toggleClass (un solo boton, esconde y aparece segun lo necesario)
+	function goAndStop(event){
+		event.preventDefault();
+		$(".progreso-bar").toggleClass("animar");
 	}
-	else{
-		$(".progreso").addClass(".animar");
+
+
+	// aqui esta la accion en dos funciones
+	function playing(event){
+		event.preventDefault();
+		$(".progreso-bar").addClass("animar");
 	}
-}
+	function stoped(event){
+		event.preventDefault();
+		$(".progreso-bar").removeClass("animar");
+	}
+
+	//la accion con un solo boton pero con una condicional comprobando si tenemos la clase
+	function playingAndStop(event){
+		event.preventDefault();
+		if ( $(".progreso-bar").hasClass("animar")) {
+			$(".progreso-bar").removeClass("animar");
+		}
+		else{
+			$(".progreso-bar").addClass("animar");
+		}
+	}
+
+
+})();
+
+
